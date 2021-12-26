f40b86fc0794463d27b6153e56d8a089535c0ba3
Katukan <notfullin.ilnaz@gmail.com>
2021-12-26 13:55:20 +0300

    added 'candy problem'

--- Tasks_ConsoleApp/Program.cs
+++ Tasks_ConsoleApp/Program.cs
@@ -4,6 +4,9 @@
 // Write a generic function chainer
 // Write_a_generic_function_chainer.Kata.Chain(Var.numberTwo, Var.funcArray);
 
-// 2) - 
+// 2) - [READY]
 // Vampire Numbers
 Vampire_Numbers.Kata.vampire_test(6, 21);
+
+// 3) 
+// Candy Problem
---
+++ Tests/3_Candy_Problem.Test.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using CandyProblem;
+
+namespace CandyProblem.Test;
+
+public class CandyProblemTest
+{
+
+	[Fact]
+	public void GetMissingCandiesTest_5_8_6_4()
+	{
+		int[] input = { 5, 8, 6, 4 };
+		int expected = 9;
+
+		int actual = CandyProblem.GetMissingCandies(input);
+
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void GetMissingCandiesTest_1_2_4_6()
+	{
+		int[] input = { 1, 2, 4, 6 };
+		int expected = 11;
+
+		int actual = CandyProblem.GetMissingCandies(input);
+
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void GetMissingCandiesTest_1_2()
+	{
+		int[] input = { 1, 2 };
+		int expected = 1;
+
+		int actual = CandyProblem.GetMissingCandies(input);
+
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void GetMissingCandiesTest_4_2()
+	{
+		int[] input = { 4, 2 };
+		int expected = 2;
+
+		int actual = CandyProblem.GetMissingCandies(input);
+
+		Assert.Equal(expected, actual);
+	}
+}
